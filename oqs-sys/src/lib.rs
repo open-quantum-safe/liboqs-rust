a32a1ef1eadaaec5dd84e818be5c8437a758fd1a
Thom Wiggers <thom@thomwiggers.nl>
2021-03-11 14:13:15 +0100

    Expose random API

--- oqs-sys/src/lib.rs
+++ oqs-sys/src/lib.rs
@@ -13,6 +13,15 @@ pub mod common {
 #[allow(non_upper_case_globals)]
 #[allow(non_camel_case_types)]
 #[allow(non_snake_case)]
+pub mod rand {
+    pub use super::common::OQS_STATUS;
+    include!(concat!(env!("OUT_DIR"), "/rand_bindings.rs"));
+}
+
+#[allow(clippy::all)]
+#[allow(non_upper_case_globals)]
+#[allow(non_camel_case_types)]
+#[allow(non_snake_case)]
 pub mod kem {
     pub use super::common::OQS_STATUS;
     include!(concat!(env!("OUT_DIR"), "/kem_bindings.rs"));
